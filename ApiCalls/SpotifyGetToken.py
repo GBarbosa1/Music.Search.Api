44cc92f0285a0c27c4c962d380e3630b2d39fa7b
GBarbosa1 <barbosa.gbsantos@gmail.com>
2023-08-25 23:16:01 -0300

    Added the token and search classes

--- ApiCalls/SpotifyGetToken.py
+++ ApiCalls/SpotifyGetToken.py
@@ -1,24 +1,46 @@
 import requests
 from dotenv import load_dotenv
+import os
 load_dotenv('.env')
-class SpotifyToken:
+
+#Pode ser que volte trackName igual mas URI diferentes
+#Definir limit de returns
+class SpotifyOperator:
 
   def __init__(self):
     self.token = None
+    self.uri = None
+    self.music = None
 
   def getToken(self):
-    
-    url = "https://accounts.spotify.com/api/token"
-    payload = 'grant_type=client_credentials'
+    url = os.environ['SPOTIFY-TOKEN-URL']
+    payload = os.environ['SPOTIFY-TOKEN-PAYLOAD']
     headers = {
-      'Authorization': 'Basic ',
-      'Content-Type': 'application/x-www-form-urlencoded'
+      'Authorization': os.environ['SPOTIFY-CREDENTIALS-BASE64'],
+      'Content-Type': os.environ['SPOTIFY-CONTENT-TYPE']
     }
 
     response = requests.request("POST", url, headers=headers, data=payload).json()
     self.token = response['access_token']
 
+  def getTrackUri(self):
+    trackNameList = []
+    trackUriList = []
+    url = os.environ['SPOTIFY-URI-URL']
+    payload = {}
+    params = {"query":"baile do cinga", "type":"track", "market":"US","limit":"5", "offset":"0"}
+    headers = {'Authorization': "Bearer " + str(self.token)}
+    response = requests.request("get", url, headers=headers, params=params, data=payload).json()
+    response = response['tracks']['items']
+    for x in range(len(response)):
+      trackNameToList = response[x]['name']
+      trackUriToList = response[x]['uri']
+      trackNameList.append(trackNameToList)
+      trackUriList.append(trackUriToList)
+    self.tracks = trackNameList
+    self.uri = trackUriList
 
-token = SpotifyToken()
-token.getToken()
-print(token.token)
+spot = SpotifyOperator()
+spot.getToken()
+spot.getTrackUri()
+print(spot.tracks, spot.uri)
